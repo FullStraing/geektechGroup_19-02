a7e8a88fd330a1a74793a9d957c328bd4315b391
flex-oops <94053950+FullStraing@users.noreply.github.com>
2022-10-27 18:57:40 +0600

    Update app.js

--- lesson_6/login/app.js
+++ lesson_6/login/app.js
@@ -1,35 +1,40 @@
 const form = document.querySelector("form");
 const inputs = form.elements;
 
-const users = [
-  ["oops", "Hello123"],
-  ["admin", "adminTop"],
-  ["geektech", "12345"],
-  ["Salam", "new-York"]
-];
 
-function isValid(username, password) {   
-  for(var i = 0; i < users.length; i++) {
-      var temp = users[i];
-      if (temp[0] == username) {
-        var currentPair = temp;
-        break;        
-      }        
-  }     
-  if (currentPair && currentPair[1] == password) {
-    return true;           
-  } else {
-    return false;
-  }
-}
 
-form.addEventListener('submit', function(e){
-  e.preventDefault();
-  var username = inputs["username"].value;
-  var password = inputs["password"].value;  
-    if (!isValid(username, password)){        
-      alert('Неверный логин или пароль');
-    } else {      
-      alert('Добро пожаловать');
+const users= [
+  {
+    user:'admin',
+    password:'KoI18'
+  },
+  {
+    user:'manager',
+    password:'SuperMe108'
+  },
+  {
+    user:'editor',
+    password:'12345'
+  },
+]
+
+
+
+function check(user,password){
+    users.map((key)=>{
+    if(key.user===user){
+      if(key.password===password){
+        alert('Данные верные')
+        inputs[0].value = ''
+        inputs[1].value = ''
+      }else{
+        alert('Логин или пароль неверный')
+      }
     }
+})
+
+
+form.addEventListener('submit',(e)=>{
+  e.preventDefault()
+  check(inputs[0].value,inputs[1].value)
 });
